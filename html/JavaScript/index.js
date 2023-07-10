fc1ea2fe82a06f0201acb19305ca90acc82f5de0
applicants-backend <tyser@naver.com>
2023-07-10 16:51:58 +0900

    css,html,js

---
+++ html/JavaScript/index.js
@@ -0,0 +1,40 @@
+console.log("Hello world")
+
+// 변수란 특정 값을 저장하는 공간.
+// abc = "abcdefgh---"
+
+// 키워드 변수이름 = 값.
+// 키워드 : var , let , const 
+
+var number = 5; // 변수의 선언과 할당이 동시에 일어난다.
+var number; // 재선언
+
+// var 보다 let 사용을 권장한다.
+// for () {
+
+// }
+
+// function aaa {
+//     var string = "가나다";
+// }
+
+var number1; // 변수를 선언한다. 
+number1 = 5; // 변수의 값을 할당한다.
+number1 = 6; // 재 할당, 값을 다시 설정해준다.
+
+// var let const 차이점.
+// var : 재선어언, 재할당이 가능하다. 
+
+let string;
+let string1 = "따옴표 처리 필수" ;
+
+// let : 재선언 불가능,재할당은 가능.
+
+const Name = "이름";
+const Name1 = "Stirng2";
+console.log(Name);
+
+let lll;
+console.log(lll);
+
+// const : 재선언 불가능, 재할당도 불가능
