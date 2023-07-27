96915b2a3ae7c087f0469f02f07d35ec0ac06e85
applicants-backend <tyser@naver.com>
2023-07-27 20:51:37 +0900

    node

---
+++ NodeJs/index.js
@@ -0,0 +1,31 @@
+// const mod = require("./module")
+// console.log(mod)
+
+// const {a,b} = require('./module')
+// console.log(b)
+
+const http = require('http');
+
+const fs = require('fs').promises;
+
+//req 요청 es 응답
+const server = http.createServer(function(req, res) {
+    // res.writeHead(200)
+    // res.write(" <h1> Hello World! </h1> ")
+    // res.end(" <p> End <p> ")
+    // 파일 전송
+    try {
+        const data = fs.readFileSync('./index.html');
+        res.writeHead(200);
+        res.end(data);
+
+    } catch (error) {
+        console.log(err);
+        res.writeHead(404);
+        res.end(err.message);
+    }
+});
+
+server.listen(8000, function() {
+    console.log('8000번 포트로 실행')
+}); 
---
+++ NodeJs/module.js
@@ -0,0 +1,9 @@
+const abc = "a 변수";
+const b = "b 변수";
+
+module.exports = {a : abc, byc : b}
+
+// {
+//     a: "a 변수"
+//     b: "b 변수"
+// }
