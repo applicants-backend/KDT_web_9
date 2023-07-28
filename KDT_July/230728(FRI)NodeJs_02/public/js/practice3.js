914a5df1b427e95bb00def8779b4d9693f026d0f
applicants-backend <tyser@naver.com>
2023-07-28 20:05:29 +0900

    node js

---
+++ KDT_July/230728(FRI)NodeJs_02/public/js/practice3.js
@@ -0,0 +1,14 @@
+
+for(i = 1; i <= 9; i++) {
+    console.log("--------" + i + "단 입니다 -----------")
+    for(j = 1; j <= 9; j++){
+        console.log(i + " x " + j +  " = "+ i * j)
+    }
+}
+
+for(let i=1; i<=9 ; i++) {
+    console.log(`---${i} 단---`)
+    for(let j=1; j <=9; j++){
+        console.log(`${i} x ${j} = ${i*j}`)
+    }
+}
