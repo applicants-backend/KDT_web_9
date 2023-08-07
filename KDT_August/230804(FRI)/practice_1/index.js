5626c5d8ee36680b3a9f72ceb7f3704ac6c1c5e5
applicants-backend <tyser@naver.com>
2023-08-07 17:11:51 +0900

    mvc

--- KDT_August/230804(FRI)/practice_1/index.js
+++ KDT_August/230804(FRI)/practice_1/index.js
@@ -5,17 +5,12 @@ const PORT = 8000;
 //body-parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 //view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 const router = require('./routes');
 app.use('/', router);
-
-app.get('*', (req,res) => {
-    res.render('404');
-})
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
