829048630c03adbc2901ace960e7c93c03a1bba3
applicants-backend <tyser@naver.com>
2023-08-07 17:56:29 +0900

    mvc, mysql

--- KDT_August/230804(FRI)/mvc_mysql/index.js
+++ KDT_August/230804(FRI)/mvc_mysql/index.js
@@ -1,25 +1,20 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const PORT = 8000;
 
-//view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
-//body-parser
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const indexRouter = require('./router')
-app.use('/',router);
+const indexRouter = require('./routes');
+app.use('/', indexRouter);
 
-
-app.get('*', (req,res) => {
+app.use('*', (req, res) => {
     res.render('404');
-})
+});
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
-
 
