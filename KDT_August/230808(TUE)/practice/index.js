b444944214c5b291eb5739b47622aa883cf81608
applicants-backend <tyser@naver.com>
2023-08-09 15:55:12 +0900

    signin,up, profile

--- KDT_August/230808(TUE)/practice/index.js
+++ KDT_August/230808(TUE)/practice/index.js
@@ -8,14 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //router
-// const indexRouter = require('./routes');
-// app.use('/', indexRouter);
-const visitorRouter = require('./routes/visitor');
-app.get('/', (req, res) => {
-    res.render('index');
-});
-//localhost:8000/visitor
-app.use('/visitor', visitorRouter);
+
+const userRouter = require('./routes/user');
+app.use('/user', userRouter);
 
 app.use('*', (req, res) => {
     res.render('404');
