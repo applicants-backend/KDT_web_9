bf972dd6c01bc17efc67789f9881739c48dee186
applicants-backend <tyser@naver.com>
2023-08-10 20:30:12 +0900

    m

---
+++ KDT_August/230810(THU)/backend_project/models/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+const Sequelize = require('sequelize');
+const config = require(__dirname + '/../config/config.json')[development];
+const db = {};
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+// model
+db.Student = require('./Student')(sequelize)
+db.Classes = require('./Classes')(sequelize)
+
+// create relationship
+db.Student.hasMany(db.Classes, {foreignKey:'student_id'})
+db.Classes.hasMany(db.Student, {foreignKey:'student_id'})
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
